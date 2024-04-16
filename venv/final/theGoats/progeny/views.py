6c7c7984a3807baeefc182866478ad9c40a706c1
BobBjorklund <bjorklr1@tcnj.edu>
2024-04-15 22:48:58 -0400

    initial django routing

--- venv/final/theGoats/progeny/views.py
+++ venv/final/theGoats/progeny/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# from django.template import render
 # Create your views here.
 def index(request):
-    return HttpResponse('PROGENY MOFRACKIE')
+    return render(request,'progeny/index.html',{})
--- venv/final/theGoats/theGoats/views.py
+++ venv/final/theGoats/theGoats/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from progeny import urls
+from seasons import urls
 # Create your views here.
 def index(request):
-    return HttpResponse('index')
+    links = {'The Progeny Report':'pindex','The Seasonal Birth Cohort Report':'sindex'}
+    return render(request,'theGoats/index.html',context={'links':links})
