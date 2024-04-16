7b68b10a66300e08130d55b5f42fed8c33571d52
BobBjorklund <bjorklr1@tcnj.edu>
2024-04-15 23:39:39 -0400

    working out record passing

--- venv/final/theGoats/progeny/views.py
+++ venv/final/theGoats/progeny/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # from django.template import render
 # Create your views here.
+import psycopg2
 def index(request):
-    return render(request,'progeny/index.html',{})
+    connection = psycopg2.connect(database="goats", user="lion", password="lion", host="localhost", port=5432)
+    cursor = connection.cursor()
+    q = 'Select * from damwbw;'
+    cursor.execute(q)
+    dams = cursor.fetchall()
+    return render(request,'progeny/index.html',{'dams':dams})
