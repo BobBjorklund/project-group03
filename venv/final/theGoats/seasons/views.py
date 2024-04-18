b7293337d00630b2dac762b71c878fb19c8baec5
BobBjorklund <bjorklr1@tcnj.edu>
2024-04-18 14:21:03 -0400

    basic seasons framework

--- venv/final/theGoats/seasons/views.py
+++ venv/final/theGoats/seasons/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import psycopg2
+from psycopg2 import extras
 # Create your views here.
 def index(request):
+    connection = psycopg2.connect(database="goats", user="lion", password="lion", host="localhost", port=5432)
+    curr = connection.cursor(cursor_factory = psycopg2.extras.DictCursor)
+    q = 'select * from season;'
+    curr.execute(q)
+    s = curr.fetchall()
+    s = [season for season in s]
     context={
-        
+        's':s
     }
     return render(request,'seasons/index.html',context)
 # Create your views here.
